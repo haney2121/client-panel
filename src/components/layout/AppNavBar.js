da546fe69cac865ba31c3611eea0ed2380175996
Haney2121 <haney037332@gmail.com>
2018-11-05 16:34:34 -0600

    first Commit

---
+++ src/components/layout/AppNavBar.js
@@ -0,0 +1,37 @@
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+
+class AppNavBar extends Component {
+  render() {
+    return (
+      <Fragment>
+        <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
+          <div className="container">
+            <Link to="/" className="navbar-brand">
+              Client Panel
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarMain"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarMain">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">
+                    Dashboard
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </Fragment>
+    );
+  }
+}
+
+export default AppNavBar;
